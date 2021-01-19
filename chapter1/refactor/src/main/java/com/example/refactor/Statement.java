1f01e7947c1d5e15dd70663d8191aac5962259bc
ke <keliu@Kes-MacBook-Pro.local>
2021-01-19 18:03:40 +0800

    0.1

---
+++ chapter1/refactor/src/main/java/com/example/refactor/Statement.java
@@ -0,0 +1,12 @@
+package com.example.refactor;
+
+public class Statement {
+    public String statement(double invoice, String plays){
+        int totalAmout = 0;
+        int volumeCredits = 0;
+
+        return "1";
+    }
+
+
+}
